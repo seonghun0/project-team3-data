a1d1ac007de4385fb1ce31bf1c5a9c148864d08a
skwpp <90810931+skwpp@users.noreply.github.com>
2021-12-10 13:18:34 +0900

    add

---
+++ modules/contents_recommendation_system.py
@@ -0,0 +1,212 @@
+def contents_recommendation_system(output_path):
+
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    from scipy import stats
+    from ast import literal_eval
+    from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+    from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
+    from nltk.stem.snowball import SnowballStemmer
+    from nltk.stem.wordnet import WordNetLemmatizer
+    from nltk.corpus import wordnet
+    from surprise import Reader, Dataset, SVD, accuracy
+    import warnings; warnings.simplefilter('ignore')
+
+    links_small = pd.read_csv("../data-files/recommendation/links_small.csv")
+    links_small = links_small[links_small['tmdbId'].notnull()]['tmdbId'].astype('int')
+    links_small.head()
+
+    # Drop a row by index : 19730, 29503, 33587 행은 이상한 데이터들(md.iloc[19730], md.iloc[29503], md.iloc[33587])
+    md = md.drop([19730, 29503, 35587])
+
+    #Check EDA Notebook for how and why I got these indices.
+    md['id'] = md['id'].astype('int')
+
+
+    smd = md[md['id'].isin(links_small)]
+    smd.shape
+
+
+    smd['tagline'] = smd['tagline'].fillna('')
+    smd['description'] = smd['overview'] + smd['tagline']
+    smd['description'] = smd['description'].fillna('')
+
+    smd['description'].head()
+
+
+    tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 2), min_df=0, stop_words='english')
+    tfidf_matrix = tf.fit_transform(smd['description'])
+
+
+    print(tfidf_matrix[10])
+
+
+    tfidf_matrix.shape
+
+
+    # linear_kernel는 두 벡터의 dot product 이다.
+    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+
+    cosine_sim[0]
+
+
+    smd = smd.reset_index()
+    titles = smd['title']
+    indices = pd.Series(smd.index, index=smd['title'])
+
+    print(titles.head(), indices.head())
+
+
+    def get_recommendations(title):
+        idx = indices[title]
+        sim_scores = list(enumerate(cosine_sim[idx]))
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = sim_scores[1:31]
+        movie_indices = [i[0] for i in sim_scores]
+        return titles.iloc[movie_indices]
+        get_recommendations('The Godfather').head(10)
+
+        # The Dark Knight
+        get_recommendations('Inception').head(10)
+
+
+    credits = pd.read_csv('./drive/MyDrive/data-files/credits.csv')
+    keywords = pd.read_csv('./drive/MyDrive/data-files/keywords.csv')
+
+
+    credits['crew'][0]
+
+
+    keywords['id'] = keywords['id'].astype('int')
+    credits['id'] = credits['id'].astype('int')
+    md['id'] = md['id'].astype('int')
+
+
+    md.shape
+
+
+    md = md.merge(credits, on='id')
+    md = md.merge(keywords, on='id')
+
+
+    smd = md[md['id'].isin(links_small)]
+    smd.shape
+
+
+    smd['cast'] = smd['cast'].apply(literal_eval)
+    smd['crew'] = smd['crew'].apply(literal_eval)
+    smd['keywords'] = smd['keywords'].apply(literal_eval)
+    smd['cast_size'] = smd['cast'].apply(lambda x: len(x))
+    smd['crew_size'] = smd['crew'].apply(lambda x: len(x))
+
+
+    def get_director(x):
+        for i in x:
+            if i['job'] == 'Director':
+                return i['name']
+        return np.nan
+        smd['director'] = smd['crew'].apply(get_director)
+
+
+    # 출연진 중 상위에 노출되는 3명만 추출
+    smd['cast'] = smd['cast'].apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+    smd['cast'] = smd['cast'].apply(lambda x: x[:3] if len(x) >= 3 else x)
+
+
+    smd['keywords'] = smd['keywords'].apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+
+
+    # 출연진의 이름에서 공백 삭제
+    smd['cast'] = smd['cast'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+
+
+    # 감독의 이름에서 공백 삭제 및 3번 언급?
+    smd['director'] = smd['director'].astype('str').apply(lambda x: str.lower(x.replace(" ", "")))
+    smd['director'] = smd['director'].apply(lambda x: [x, x, x])
+
+
+    s = smd.apply(lambda x: pd.Series(x['keywords']), axis=1).stack().reset_index(level=1, drop=True)
+    s.name = 'keyword'
+
+
+    s = s.value_counts()
+    s[:5]
+
+
+    # 2번 이상 등장한 키워드만 추출
+    s = s[s > 1]
+
+
+    # 어근 추출을 통해 동일 의미&다른 형태의 단어(dogs&dog, imaging&image 등)를 동일한 단어로 인식
+    stemmer = SnowballStemmer('english')
+    print("dogs의 어근 : ", stemmer.stem('dogs'))
+    print("dog의 어근 : ", stemmer.stem('dog'))
+
+
+    def filter_keywords(x):
+        words = []
+        for i in x:
+            if i in s:
+                words.append(i)
+        return words
+
+    # 키워드의 어근을 찾아서 공백 제거 후 세팅
+    smd['keywords'] = smd['keywords'].apply(filter_keywords)
+    smd['keywords'] = smd['keywords'].apply(lambda x: [stemmer.stem(i) for i in x])
+    smd['keywords'] = smd['keywords'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+
+
+    smd['soup'] = smd['keywords'] + smd['cast'] + smd['director'] + smd['genres']
+    smd['soup'] = smd['soup'].apply(lambda x: ' '.join(x))
+
+
+    count = CountVectorizer(analyzer='word', ngram_range=(1,2), min_df=0, stop_words='english')
+    count_matrix = count.fit_transform(smd['soup'])
+
+
+    cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+
+    smd = smd.reset_index()
+    titles = smd['title']
+    indices = pd.Series(smd.index, index=smd['title'])
+
+
+    get_recommendations('The Dark Knight').head(10)
+
+
+    get_recommendations('Mean Girls').head(10)
+
+
+    def improved_recommendations(title):
+        print(title)
+        idx = indices[title]
+        print(idx)
+        sim_scores = list(enumerate(cosine_sim[idx]))
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = sim_scores[1:26]
+        movie_indices = [i[0] for i in sim_scores]
+        print(movie_indices)
+
+        movies = smd.iloc[movie_indices][['title','vote_count','vote_average','year']]
+        # print(movies)
+        
+        vote_counts = movies[movies['vote_count'].notnull()]['vote_count'].astype('int')
+        vote_averages = movies[movies['vote_average'].notnull()]['vote_average'].astype('int')
+        C = vote_averages.mean()
+        m = vote_counts.quantile(0.60)
+        qualified = movies[(movies['vote_count'] >= m) & (movies['vote_count'].notnull())]
+        # print(qualified)
+        qualified['vote_count'] = qualified['vote_count'].astype('int')
+        qualified['wr'] = qualified.apply(weighted_rating, axis=1)
+        qualified = qualified.sort_values('wr', ascending=False).head(10)
+        print(qualified)
+        return qualified
+        improved_recommendations('The Dark Knight')
+        improved_recommendations('Mean Girls')
+
+if __name__ == "__main__":
+   pass
